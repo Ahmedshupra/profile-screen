2d2e87e72515d8b3a7def48984d8a16bae9a46cc
Ahmedshupra <122183907+Ahmedshupra@users.noreply.github.com>
2023-11-29 03:19:26 +0200

    change task name

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter2/quistion-1.dart';
+import 'package:flutter2/profile_screen.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Quistion_1(),
+      home: ProfileScreen(),
     )
   );
 }
--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:icons_plus/icons_plus.dart';
 
-class Quistion_1 extends StatelessWidget {
-  const Quistion_1({super.key});
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
